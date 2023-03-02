4af8434e73a8cd6717872100a2e43a47899304ee
gbyrne1 <byrneg3@newpaltz.edu>
2023-03-02 14:09:57 -0500

    added navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import type NavbarVue from './components/Navbar.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<NavbarVue />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+ 
+div class="container">
   <RouterView />
+</div>
+
 </template>
 
 <style scoped> </style>

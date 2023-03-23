11e2ef94ec60c7aef980cc1aee33a5de23db1b0f
gbyrne1 <byrneg3@newpaltz.edu>
2023-03-23 14:25:49 -0400

    added build script to package.json

--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
 const express = require('express')
+const path = require('path')
 const products = require('./controllers/products')
 const app = express()
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-
+// Middleware
 app
-    .get('/', (req, res) => {
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
+
+
+// Actions
+app
+    .get('/api/v1/', (req, res) => {
         res.send('Hello World! From Express')
     })
-    .use('/products', products)
+    .use('/api/v1/products', products)
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
 
 
 

2bf60bccac7f343ed7927b2d417a36278d71ab10
gbyrne1 <byrneg3@newpaltz.edu>
2023-03-30 14:26:19 -0400

    added Notifications component and api wrapper

---
+++ client/src/components/Notifications.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useSession } from '@/model/session';
+import { ref } from 'vue';
+const session= useSession();
+const isActive= ref(false);
+</script>
+
+<template>
+    <div class="notifications">
+<button class="button is-primary" @click="isActive=!isActive">
+<span class="icon">
+    <i class="fas fa-bell fa-lg"></i>
+    </span>
+</button>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
--- client/src/model/session.ts
+++ client/src/model/session.ts
@@ -1,9 +1,14 @@
-
 import { reactive } from "vue";
 import { useRouter } from "vue-router";
+import * as myFetch from "./myFetch";
 
 const session = reactive({
     user: null as User | null,
+    isLoading: false,
+    messages: [] as {
+        msg: string,
+        type: "success" | "error" | "warning" | "info",
+    }[],
 })
 
 interface User {
@@ -18,17 +23,34 @@ export function useSession() {
     return session;
 }
 
+export function api(url: string) {
+    session.isLoading = true;
+    return myFetch.api(url)
+        .catch(err => {
+            console.error(err);
+            session.messages.push({
+                msg: err.message ?? JSON.stringify(err),
+                type: "error",
+            })
+        })
+        .finally(() => {
+            session.isLoading = false;
+        })
+}
+
 export function login() {
     session.user = {
         name: "John Doe",
     }
 }
 
-export function uselogout() {
-    const router= useRouter();
-   return function(){
-    console.log({router});
-    session.user = null;
+export function useLogout() {
+    const router = useRouter();
+    
+    return function(){
+        console.log({router});
+        session.user = null;
 
-    router.push("/login");}
+        router.push("/login");
+    }
 }

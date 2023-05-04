291ab41b7ce40e35a58beac001eb06be22b89de9
gbyrne1 <byrneg3@newpaltz.edu>
2023-05-03 22:22:28 -0400

    test

--- client/src/model/products.ts
+++ client/src/model/products.ts
@@ -1,6 +1,7 @@
 //(default export)when importing a component dont put in squigglies{}, can be named whatever you want
 //if their are named exports, this doesnt apply, use squigglies{} and exact name
 
+import type { DataListEnvelope } from "./myFetch";
 import { api } from "./session";
 
 export interface Product {
@@ -17,7 +18,7 @@ export interface Product {
     images: string[];
 }
 
-export function getProducts(): Promise<Product[]> {
+export function getProducts(): Promise<DataListEnvelope<Product>> {
 
     return api('products')
 
